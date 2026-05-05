2b75bee4f72b922acd9b97f712bbcc0f1fa683a3
Your Name <you@example.com>
2026-05-05 20:52:31 +0100

    small changes

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_whatthefuck.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamendes <mamendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 23:15:04 by mamendes          #+#    #+#             */
+/*   Updated: 2026/05/05 20:51:05 by mamendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+
+	if (s == NULL)
+		return (NULL);
+	dest = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (dest == NULL)
+		return (NULL);
+	ft_strlcpy(dest, s, ft_strlen(s) + 1);
+	return (dest);
+}
+                                                                                                                                                                                                                                                                                                  
+/* #include <stdio.h>
+
+int main()
+{
+	char *str = "ola amigos";
+	char *dest = ft_strdup(str);
+	printf("%s\n", dest);
+	free(dest);
+} */
